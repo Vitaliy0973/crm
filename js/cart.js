ed4d660200978d27f51e3dae11ec3266dd828cba
Vitaliy0973 <arkodianec1@gmail.com>
2023-09-15 14:12:47 +0300

    task comleted

--- js/cart.js
+++ js/cart.js
@@ -60,10 +60,15 @@ import { goods } from "./goods.js";
 }
 
 const addProductModal = document.querySelector('.overlay');
+const addProductForm = document.querySelector('.modal__form');
 const cartTableBody = document.querySelector('.table__body');
 const addProductButton = document.querySelector('.panel__add-goods');
+const cartTotalPrice = document.querySelector('.cms__total-price');
 
 const createRow = (obj, count) => {
+  const totalPrice = obj.count * obj.price - obj.count * obj.price *
+    obj.discount / 100 ?? obj.count * obj.price;
+
   const tableRow = createElement('tr', {}, {
     appends: [
       createElement('td', {
@@ -93,7 +98,7 @@ const createRow = (obj, count) => {
       }),
       createElement('td', {
         className: 'table__cell',
-        textContent: `$${obj.count * obj.price}`,
+        textContent: `$${totalPrice}`,
       }),
       createElement('td', {
         className: 'table__cell table__cell_btn-wrapper',
@@ -115,50 +120,149 @@ const createRow = (obj, count) => {
   });
 
   return tableRow;
-}
+};
 
 const renderGoods = (arr, tableBody) => {
   arr.forEach((item, index) => {
     tableBody.insertAdjacentElement('beforeend', createRow(item, index + 1));
   });
-}
+};
 
 const clearTable = (tableBody) => {
   while (tableBody.firstChild) {
     tableBody.firstChild.remove();
   }
+};
+
+const closeModal = () => {
+  addProductModal.classList.remove('active');
+
+  if (addProductForm.discount.checked) {
+    addProductForm.discount_count.setAttribute('disabled', '');
+  };
+
+  addProductForm.reset();
 }
 
-addProductModal.classList.remove('active');
+const getRandomId = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const calculateTotalPrice = () => {
+  let totalPrice = addProductForm.price.value * addProductForm.count.value;
+
+  if (addProductForm.discount.checked && addProductForm.discount_count.value) {
+    return totalPrice - (totalPrice * addProductForm.discount_count.value / 100);
+  }
+  return totalPrice;
+};
+
+const createProductData = (data, id) => {
+  return {
+    "id": id,
+    "title": data.name,
+    "price": +data.price,
+    "description": data.description,
+    "category": data.category,
+    "count": +data.count,
+    "units": data.units,
+    "discount": data.hasOwnProperty('discount') ? +data.discount_count : false,
+    "images": {
+      "small": "img/product-small.jpg",
+      "big": "img/produc-big.jpg"
+    }
+  }
+};
+
+const addProductData = (data) => {
+  goods.push(data);
+}
+
+const addProductPage = (data, tableBody) => {
+  tableBody.append(createRow(data, tableBody.rows.length + 1));
+};
+
+const pageTotalPrice = () => {
+  const totalPrice = goods.reduce((sum, item) => {
+    return sum += item.price * item.count - item.price * item.count *
+      item.discount / 100 ?? item.price * item.count;
+  }, 0);
+
+  cartTotalPrice.textContent = `$ ${totalPrice}`;
+};
+
+
+closeModal()
+clearTable(cartTableBody);
+renderGoods(goods, cartTableBody);
+pageTotalPrice();
+
 
 addProductButton.addEventListener('click', () => {
+  const productId = addProductModal.querySelector('.vendor-code__id');
+  productId.textContent = getRandomId(1, 99999999999999);
   addProductModal.classList.add('active');
 });
 
 addProductModal.addEventListener('click', (e) => {
   if (e.target === addProductModal || e.target.closest('.modal__close')) {
-    addProductModal.classList.remove('active');
+    closeModal();
   }
 });
 
-clearTable(cartTableBody);
-renderGoods(goods, cartTableBody);
-
 cartTableBody.addEventListener('click', (e) => {
-  const cartDelProduct = Array.from(document.querySelectorAll('.table__btn_del'));
+  const cartDelProduct = Array.from(cartTableBody.querySelectorAll('.table__btn_del'));
 
   if (cartDelProduct.includes(e.target)) {
     const row = e.target.closest('tr');
     const id = row.querySelector('.table__cell_name').dataset.id;
+
     row.remove();
+
+    Array.from(cartTableBody.rows).forEach((item, index) => {
+      item.firstChild.textContent = index + 1;
+    });
 
     goods.forEach((item, index) => {
       if (item.id === +id) {
         goods.splice(index, 1);
       }
-    })
-  }
+    });
 
-  console.log(goods);
+    pageTotalPrice();
+  }
 });
 
+addProductForm.addEventListener('change', e => {
+  if (e.target === addProductForm.discount) {
+    if (addProductForm.discount_count.hasAttribute('disabled')) {
+      addProductForm.discount_count.removeAttribute('disabled');
+    } else {
+      addProductForm.discount_count.setAttribute('disabled', '');
+      addProductForm.discount_count.value = '';
+    }
+  }
+
+  if (e.target === addProductForm.price || e.target === addProductForm.count ||
+    e.target === addProductForm.discount_count || e.target === addProductForm.discount) {
+    addProductForm.total.value = `$ ${calculateTotalPrice()}`;
+  };
+});
+
+addProductForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const newProduct = Object.fromEntries(formData);
+  const newProductId = +(addProductModal.querySelector('.vendor-code__id').textContent);
+  const productData = createProductData(newProduct, newProductId);
+
+  addProductData(productData);
+  addProductPage(productData, cartTableBody);
+  pageTotalPrice();
+
+  closeModal();
+  e.target.reset();
+});
--- js/script.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const addProductTitle = document.querySelector('.modal__title');
-const addProductForm = document.querySelector('.modal__form');
-const addProductDiscountCheckbox = addProductForm.querySelector('.modal__checkbox');
-const addProductDiscountName = addProductForm.querySelector('.modal__input_discount');
